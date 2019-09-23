b7bfb6a27f150c5d21634b50205bffa22e4d5ac0
Ethan B <ethanbuss.work@gmail.com>
2019-09-23 10:28:24 -0700

    Update Basic_JavaScript_1.js

--- Basic Javascript Projects/JS/Basic_JavaScript_1.js
+++ Basic Javascript Projects/JS/Basic_JavaScript_1.js
@@ -3,3 +3,21 @@ document.write("Hello, World!")
 var A = "This is a string";
 
 document.write(A)
+
+document.write("\"Hello, World\"")
+
+document.write("Welcome to the Jumgle"
+ + " We\'ve got fun and games")
+
+ var C = "Concatenated" + " String"
+ document.write(C)
+
+ var Family = "The Buss\'"; Dad = "Ben"; Mum ="Alice"; EldestBoy = "Ethan"; YoungestBoy = "Jared";
+ var Family = Family.fontcolor("Green");
+ var Dad = Dad.fontcolor("Red");
+ var Mum = Mum.fontcolor("Blue");
+ var EldestBoy = EldestBoy.fontcolor("Yellow");
+ var YoungestBoy = YoungestBoy.fontcolor("Orange");
+document.write(EldestBoy)
+
+document.write(5+6)
